a423dd07443b7120b1b9c98ba4abb77c00cd216a
Jan Dočkal <dockal07@gmail.com>
2019-03-06 12:28:43 +1300

    Link pages to main App

--- src/Web/App.tsx
+++ src/Web/App.tsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
+import { CartContext } from './Context/CartContext';
+import { HomePage } from './Containers/Pages/HomePage';
+import { CartPage } from './Containers/Pages/CartPage';
+import { Header } from './Containers/Header/Header';
+import { Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './Model/theme';
+const products = require('../data/products.json');
 
 export interface IAppProps {
 	config: {
@@ -8,7 +16,19 @@ export interface IAppProps {
 
 const App = (_props: IAppProps) => {
 	return (
-		<h1>Hello</h1>
+		<ThemeProvider theme={theme}>
+			<CartContext>
+				<Header/>
+				<Switch>
+					<Route path="/" exact={true}>
+						<HomePage products={products} />
+					</Route>
+					<Route path="/cart" exact={true}>
+						<CartPage/>
+					</Route>
+				</Switch>
+			</CartContext>
+		</ThemeProvider>
 	);
 };
 
